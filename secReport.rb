6a4b8bd790718138d001f418072f65d3748d19ec
root <root@puppetm.localdomain>
2013-04-02 13:21:14 +0200

    shell scripting done in Ruby - yay

---
+++ secReport.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+# a quick script to generate statistics on a debian system
+# based on debsecan(1)
+# this is extremely slow btw - need to optimize it 
+# 6 calls to debsecan? insane!!!
+$COMMAND = "debsecan --suite squeeze"
+high 	= `#{$COMMAND} |grep high |wc -l`
+highr	= `#{$COMMAND} |grep high |grep remotely |wc -l`
+medium 	= `#{$COMMAND} |grep medium |wc -l`
+mediumr	= `#{$COMMAND} |grep medium |grep remotely |wc -l`
+low 	= `#{$COMMAND} |grep low |wc -l`
+lowr	= `#{$COMMAND} |grep lowr |grep remotely |wc -l`
+
+puts "#{high.chomp} high risk vulnerabilities of which #{highr.chomp} remote"
+puts "#{medium.chomp} medium risk vulnerabilities of which #{mediumr.chomp} remote"
+puts "#{low.chomp}  low risk vulnerabilities of which #{lowr.chomp} remote"
